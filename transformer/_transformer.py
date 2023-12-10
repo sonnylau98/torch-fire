5f65f3c6eb598573bf8311dd892ad3f4a0888232
sonnylau98 <liujingze00@gmail.com>
2023-12-10 06:38:09 +0000

    Update _transformer.py

--- transformer/_transformer.py
+++ transformer/_transformer.py
@@ -29,3 +29,34 @@ class DotProductAttention(nn.Module):
       scores = torch.bmm(queries, keys.transpose(1,2)) / math.sqrt(d)
       self.attention_weights = masked_softmax(scores, valid_lens)
       return torch.bmm(self.dropout(self.attention_weights), values)
+
+class MultiHeadAttention(nn.Module):
+  def __init__(self, key_size, query_size, value_size, num_hiddens,
+               num_heads, dropout, bias=False, **kwargs):
+    super(MultiHeadAttention, self).__init__(**kwargs)
+    self.num_heads = num_heads
+    self.attention = DotProductAttention(dropout)
+    self.W_q = nn.Linear(query_size, num_hiddens, bias=bias)
+    self.W_k = nn.Linear(key_size, num_hiddens, bias=bias)
+    self.W_v = nn.Linear(value_size, num_hiddens, bias=bias)
+    self.W_o = nn.Linear(num_hiddens, num_hiddens, bias=bias)
+
+  def forward(self, queires, keys, values, valid_lens):
+    # what is transpose_qkv() ???
+    queires = transpose_qkv(self.W_q(queries), self.num_heads)
+    keys = transpose_qkv(self.W_k(keys), self.num_heads)
+    values =  transpose_qkv(self.W_v(values), self.num_heads)
+
+    # To repeat `num_heads` times.
+    if valid_lens is not None:
+      valid_lens = torch.repeat_interleave(valid_lens, repeats=self.num_heads, dim=0)
+
+    # (batch_size*num_heads, num_queries, num_hiddens/num_heads)
+    output = self.attention(queries, keys, values, valiad_lens)
+
+    # (batch_size, num_queries, num_hiddens)
+    output_concat = transpose_output(output, self.num_heads)
+    
+    return self.W_o(output_concat)
+
+
