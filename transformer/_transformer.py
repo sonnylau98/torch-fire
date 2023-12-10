6a50bf87c71d799f999664b627d330441c8bef9d
sonnylau98 <liujingze00@gmail.com>
2023-12-10 10:42:34 +0000

    Update _transformer.py

--- transformer/_transformer.py
+++ transformer/_transformer.py
@@ -206,3 +206,64 @@ class DecoderBlock(nn.Module):
     return self.addnorm3(Z, self.ffn(Z)), state
 
     
+#########################################
+
+class Decoder(nn.Module):
+    """编码器-解码器架构的基本解码器接口
+
+    Defined in :numref:`sec_encoder-decoder`"""
+    def __init__(self, **kwargs):
+        super(Decoder, self).__init__(**kwargs)
+
+    def init_state(self, enc_outputs, *args):
+        raise NotImplementedError
+
+    def forward(self, X, state):
+        raise NotImplementedError
+
+class AttentionDecoder(Decoder):
+    """带有注意力机制解码器的基本接口
+
+    Defined in :numref:`sec_seq2seq_attention`"""
+    def __init__(self, **kwargs):
+        super(AttentionDecoder, self).__init__(**kwargs)
+
+    @property
+    def attention_weights(self):
+        raise NotImplementedError
+
+#########################################
+
+class TranformerDecoder(AttentionDecoder):
+  def __init__(self, vocab_size, key_size, query_size, value_size,
+               num_hiddens, norm_shape, ffn_num_input, ffn_num_hiddens,
+               num_heads, num_layers, dropout, **kwargs):
+    super(TranformerDecoder, self).__init__(**kwargs)
+    self.num_hiddens = num_hiddens
+    self.num_layers = num_layers
+    self.embedding = nn.Embedding(vocab_size, num_hiddens)
+    self.pos_encoding = PositionalEncoding(num_hiddens, dropout)
+    self.blks = nn.Sequential()
+    for i in range(num_layers):
+      self.blks.add_module("block"+str(i), DecoderBlock(key_size, query_size, value_size, num_hiddens,
+                                                        norm_shape, ffn_num_input, ffn_num_hiddens, num_heads,
+                                                        dropout, i))
+    self.dense = nn.Linear(num_hiddens, vocab_size)
+
+  def init_state(self, enc_outputs, enc_valid_lens, *args):
+    return [enc_outputs, enc_valid_lens, [None] * self.num_layers]
+
+  def forward(self, X, state):
+    X = self.pos_encoding(self.embedding(X) * math.sqrt(self.num_hiddens))
+    self._attention_weights = [[None] * len(self.blks) for _ in range (2)]
+    for i, blk in enumerate(self.blks):
+      X, state = blk(X, state)
+      self._attention_weights[0][i] = blk.attention1.attention.attention_weights
+      self._attention_weights[1][i] = blk.attention2.attention.attention_weights
+    return self.dense(X), state
+
+  @property
+  def attention_weights(self):
+    return self._attention_weights
+
+
