52312320d4590228a3d3c197686404d456eba04d
sonnylau98 <liujingze00@gmail.com>
2023-12-10 10:02:17 +0000

    Update _transformer.py

--- transformer/_transformer.py
+++ transformer/_transformer.py
@@ -32,6 +32,7 @@ class AddNorm(nn.Module):
 
     def forward(self, X, Y):
         return self.ln(self.dropout(Y) + X)
+
 ################################################
 
 
@@ -67,13 +68,15 @@ class MultiHeadAttention(nn.Module):
     self.W_o = nn.Linear(num_hiddens, num_hiddens, bias=bias)
 
   def forward(self, queires, keys, values, valid_lens):
-    # what is transpose_qkv() ???
-    queires = transpose_qkv(self.W_q(queries), self.num_heads)
+
+    queires = transpose_qkv(self.W_q(queires), self.num_heads)
     keys = transpose_qkv(self.W_k(keys), self.num_heads)
     values =  transpose_qkv(self.W_v(values), self.num_heads)
 
     # To repeat `num_heads` times.
     if valid_lens is not None:
+
+      #Evalid_lensRROR ???
       valid_lens = torch.repeat_interleave(valid_lens, repeats=self.num_heads, dim=0)
 
     # (batch_size*num_heads, num_queries, num_hiddens/num_heads)
@@ -83,6 +86,25 @@ class MultiHeadAttention(nn.Module):
     output_concat = transpose_output(output, self.num_heads)
     
     return self.W_o(output_concat)
+
+################################################
+def transpose_qkv(X, num_heads):
+    """为了多注意力头的并行计算而变换形状
+
+    Defined in :numref:`sec_multihead-attention`"""
+    # 输入X的形状:(batch_size，查询或者“键－值”对的个数，num_hiddens)
+    # 输出X的形状:(batch_size，查询或者“键－值”对的个数，num_heads，
+    # num_hiddens/num_heads)
+    X = X.reshape(X.shape[0], X.shape[1], num_heads, -1)
+
+    # 输出X的形状:(batch_size，num_heads，查询或者“键－值”对的个数,
+    # num_hiddens/num_heads)
+    X = X.permute(0, 2, 1, 3)
+
+    # 最终输出的形状:(batch_size*num_heads,查询或者“键－值”对的个数,
+    # num_hiddens/num_heads)
+    return X.reshape(-1, X.shape[2], X.shape[3])
+################################################
 
 
 class EncoderBlock(nn.Module):
@@ -112,7 +134,7 @@ class PositionalEncoding(nn.Module):
 
     self.P = torch.zeros((1, max_len, num_hiddens))
     
-    X = torch.arange(max_lens, dtype=torch.float32).reshape(-1, 1) / torch.pow(10000, torch.arange(0, num_hiddens, 2, dtype=torch.float32) / num_hiddens)
+    X = torch.arange(max_len, dtype=torch.float32).reshape(-1, 1) / torch.pow(10000, torch.arange(0, num_hiddens, 2, dtype=torch.float32) / num_hiddens)
 
     self.P[:, :, 0::2] = torch.sin(X)
     self.P[:, :, 1::2] = torch.cos(X)
