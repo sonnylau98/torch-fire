c409d013d6611046542f290af237798e125bc98a
sonnylau98 <liujingze00@gmail.com>
2023-12-10 10:22:31 +0000

    Update _transformer.py

--- transformer/_transformer.py
+++ transformer/_transformer.py
@@ -32,7 +32,6 @@ class AddNorm(nn.Module):
 
     def forward(self, X, Y):
         return self.ln(self.dropout(Y) + X)
-
 ################################################
 
 
@@ -167,3 +166,43 @@ class TransformerEncoder(Encoder):
       X = blk(X, valid_lens)
       self.attention_weights[i] = blk.attention.attention.attention_weights
     return X
+
+
+class DecoderBlock(nn.Module):
+  def __init__(self, key_size, query_size, value_size, num_hiddens,
+               norm_shape, ffn_num_input, ffn_num_hiddens, num_heads,
+               dropout, i, **kwargs):
+    super(DecoderBlock, self).__init__(**kwargs)
+    self.i = i
+    self.attention1 = MultiHeadAttention(key_size, query_size, value_size, num_hiddens, num_heads, dropout)
+    self.addnorm1 = AddNorm(norm_shape, dropout)
+    self.attention2 = MultiHeadAttention(key_size, query_size, value_size, num_hiddens, num_heads, dropout)
+    self.addnorm2 = AddNorm(norm_shape, dropout)
+    self.ffn = PositionWiseFFN(ffn_num_input, ffn_num_hiddens, num_hiddens)
+    self.addnorm3 = AddNorm(norm_shape, dropout)
+
+  # state ???
+  def forward(self, X, state):
+    enc_outputs, enc_valid_lens = state[0],state[1]
+
+    if state[2][self.i] is None:
+      key_values = X
+    else: 
+      key_values = torch.cat((state[2][self.i], X), axis=1)
+    
+    state[2][self.i] = key_values
+
+    if self.training:
+      batch_size, num_steps, _ = X.norm_shape
+
+      dec_valid_lens = torch.arange(1, num_steps + 1. device=X.device).repeat(batch_size, 1)
+    else:
+      dec_valid_lens = None
+
+    X2 = self.attention1(X, key_values, key_values, dec_valid_lens)
+    Y = self.addnorm1(X, X2)
+    Y2 = self.attention2(Y, enc_outputs, enc_outputs, enc_valid_lens)
+    Z = self.addnorm2(Y, Y2)
+    return self.addnorm3(Z, self.ffn(Z)), state
+
+    
