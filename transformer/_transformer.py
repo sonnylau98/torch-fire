63e2f07b056820afe681e5e94a0428a8393fe0f5
sonnylau98 <liujingze00@gmail.com>
2023-12-10 08:05:43 +0000

    Update _transformer.py

--- transformer/_transformer.py
+++ transformer/_transformer.py
@@ -78,3 +78,21 @@ class EncoderBlock(nn.Module):
     Y = self.addnorm1(X, self.attention(X, X, X, valid_lens))
     return self.addnorm2(Y, self.ffn(Y))
 
+class PositionalEncoding(nn.Module):
+  def __init__(self, num_hiddens, dropout, max_len=1000):
+    super(PositionalEncoding, self).__init__()
+    self.dropout = nn.Dropout(dropout)
+
+    self.P = torch.zeros((1, max_len, num_hiddens))
+    
+    X = torch.arange(max_lens, dtype=torch.float32).reshape(-1, 1) / torch.pow(10000, torch.arange(0, num_hiddens, 2, dtype=torch.float32) / num_hiddens)
+
+    self.P[:, :, 0::2] = torch.sin(X)
+    self.P[:, :, 1::2] = torch.cos(X)
+
+
+    def forward(self, X):
+      X = X + self.P[:, :X.shape[1], :].to(X.device)
+      return self.dropout(X)
+
+
