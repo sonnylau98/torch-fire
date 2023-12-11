13a7d873092c8bae01aae4a9062ac6fec44c32c9
sonnylau98 <liujingze00@gmail.com>
2023-12-11 07:09:57 +0000

    Update transformer

--- transformer/train.py
+++ transformer/train.py
@@ -1,5 +1,109 @@
 from torch import nn
 
+import time
+
+#####################################################
+class Timer:
+    """记录多次运行时间"""
+    def __init__(self):
+        """Defined in :numref:`subsec_linear_model`"""
+        self.times = []
+        self.start()
+
+    def start(self):
+        """启动计时器"""
+        self.tik = time.time()
+
+    def stop(self):
+        """停止计时器并将时间记录在列表中"""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """返回平均时间"""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """返回时间总和"""
+        return sum(self.times)
+
+    def cumsum(self):
+        """返回累计时间"""
+        return np.array(self.times).cumsum().tolist()
+
+
+class Accumulator:
+    """在n个变量上累加"""
+    def __init__(self, n):
+        """Defined in :numref:`sec_softmax_scratch`"""
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+def grad_clipping(net, theta):
+    """裁剪梯度
+
+    Defined in :numref:`sec_rnn_scratch`"""
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+
+class Animator:
+    """在动画中绘制数据"""
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        """Defined in :numref:`sec_softmax_scratch`"""
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: d2l.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+#####################################################
+
 def train_seq2seq(net, data_iter, lr, num_epochs, tgt_vocab, device)
 
   def xavier_init_weights(m):
@@ -15,11 +119,12 @@ def train_seq2seq(net, data_iter, lr, num_epochs, tgt_vocab, device)
   optimizer = torch.optim.Adam(net.parameters(), lr=lr)
   loss = MaskedSoftmaxCELoss()
   net.train()
-  # animator
+  animator = Animator(xlabel='epoch', ylabel='loss',
+                     xlim=[10, num_epochs])
 
   for epoch in range(num_epochs):
-    timer = ########
-    metric = ########
+    timer = Timer()
+    metric = Accumulator(2)
     for batch in data_iter:
       optimizer.zero_grad()
       X, X_valid_len, Y, Y_valid_len = [x.to(device) for x in batch]
@@ -29,7 +134,7 @@ def train_seq2seq(net, data_iter, lr, num_epochs, tgt_vocab, device)
       Y_hat, _ = net(X, dec_input, X_valid_len)
       l = loss(Y_hat, Y, Y_valid_len)
       l.sum().backward()
-      # d2l.grad_clipping(net, 1)
+      grad_clipping(net, 1)
       num_tokens = Y_valid_len.sum()
       optimizer.step()
       with torch.no_grad():
