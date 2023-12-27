656dcb867eea350e1a2a324280e5feee60a9174a
sonnylau98 <liujingze00@gmail.com>
2023-12-27 10:50:27 +0000

    Update model

--- transformer/model/encoder.py
+++ transformer/model/encoder.py
@@ -1,4 +1,49 @@
-from torch.nn import MultiheadAttention
+import math
+import pandas as pd
+import torch
+from torch import nn
+
+class DotProductAttention(nn.Module):
+  def __init__(self, dropout):
+    super().__init__()
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self, queries, keys, values, valid_lens=None):
+    d = queries.shape[-1]
+    scores = torch.bmm(queries, keys.transpose(1, 2)) / math.sqrt(d)
+    self.attention_weights = masked_softmax(scores, valid_lens)
+    return torch.bmm(self.dropout(self.attention_weights), values)
+
+class MultiHeadAttention(nn.Module):
+  def __init__(self, num_hiddens,
+               num_heads, dropout, bias=False, **kwargs):
+    super().__init__()
+    self.num_heads = num_heads
+    self.attention = DotProductAttention(dropout)
+    self.W_q = nn.LazyLinear(num_hiddens, bias=bias)
+    self.W_k = nn.LazyLinear(num_hiddens, bias=bias)
+    self.W_v = nn.LazyLinear(num_hiddens, bias=bias)
+    self.W_o = nn.LazyLinear(num_hiddens, bias=bias)
+
+  def forward(self, queries, keys, values, valid_lens):
+    quries = self.transpose_qkv(self.W_q(queries))
+    keys = self.transpose_qkv(self.W_k(keys))
+    values = self.transpose_qkv(self.W_v(values))
+    if valid_lens is not None:
+      valid_lens = torch.repeat_interleave(valid_lens, repeats=self.num_heads, dim=0)
+    output = self.attention(queries, keys, values, valid_lens)
+    output_concat = self.transpose_output(output)
+    return self.W_o(output_concat)
+  
+  def transpose_qkv(self, X):
+    X = X.reshape(X.shape[0], X.shape[1], self.num_heads, -1)
+    X = X.permute(0, 2, 1, 3)
+    return X.reshape(-1, X.shape[2], X.shape[3])
+
+  def transpose_output(self, X):
+    X = X.reshape(-1, self.num_heads, X.shape[1], X.shape[2])
+    X = permute(0, 2, 1, 3)
+    return X.reshape(X.shape[0], X.shape[1], -1)
 
 class PositionWiseFFN(nn.Module):
   def __init__(self, ffn_num_hiddens, ffn_num_outputs):
@@ -23,7 +68,7 @@ class TransformerEncoderBlock(nn.Module):
   def __init__(self, num_hiddens, ffn_num_hiddens, num_heads, dropout,
                use_bias=False):
     super().__init__()
-    self.attention = MultiheadAttention(num_hiddens, num_heads,
+    self.attention = MultiHeadAttention(num_hiddens, num_heads,
                                         dropout, use_bias)
     self.addnorm1 = AddNorm(num_hiddens, dropout)
     self.ffn = PositionWiseFFN(ffn_num_hiddens, num_hiddens)
