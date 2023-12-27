ddc722ee76d344e2cc084ead2e803e4e22ea1d1a
sonnylau98 <liujingze00@gmail.com>
2023-12-27 12:47:32 +0000

    Update decoder.py

--- transformer/model/decoder.py
+++ transformer/model/decoder.py
@@ -1,3 +1,23 @@
+class Decoder(nn.Module):
+  def __init__(self, **kwargs):
+    super().__init__()
+
+  def init_state(self, enc_outputs, *args):
+    raise NotImplementedError
+
+  def forward(self, X, state):
+    raise NotImplementedError
+
+
+class AttentionDecoder(Decoder):
+  def __init__(self, **kwargs):
+    super().__init__()
+
+  @property
+  def attention_weights(self):
+    raise NotImplementedError
+
+
 class TransformerDecoderBlock(nn.Module):
   def __init__(self, num_hiddens, ffn_num_hiddens, num_heads, dropout, i):
     super().__init__()
@@ -28,3 +48,36 @@ class TransformerDecoderBlock(nn.Module):
     Y2 = self.attention2(Y, enc_outputs, enc_outputs, enc_valid_lens)
     Z = self.addnorm2(Y, Y2)
     return self.addnorm3(Z, self.ffn(Z)), state
+
+
+class TransformerDecoder(AttentionDecoder):
+    def __init__(self, vocab_size, num_hiddens, ffn_num_hiddens, num_heads,
+                 num_blks, dropout):
+        super().__init__()
+        self.num_hiddens = num_hiddens
+        self.num_blks = num_blks
+        self.embedding = nn.Embedding(vocab_size, num_hiddens)
+        self.pos_encoding = d2l.PositionalEncoding(num_hiddens, dropout)
+        self.blks = nn.Sequential()
+        for i in range(num_blks):
+            self.blks.add_module("block"+str(i), TransformerDecoderBlock(
+                num_hiddens, ffn_num_hiddens, num_heads, dropout, i))
+        self.dense = nn.LazyLinear(vocab_size)
+
+    def init_state(self, enc_outputs, enc_valid_lens):
+        return [enc_outputs, enc_valid_lens, [None] * self.num_blks]
+
+    def forward(self, X, state):
+        X = self.pos_encoding(self.embedding(X) * math.sqrt(self.num_hiddens))
+        self._attention_weights = [[None] * len(self.blks) for _ in range (2)]
+        for i, blk in enumerate(self.blks):
+            X, state = blk(X, state)
+            self._attention_weights[0][
+                i] = blk.attention1.attention.attention_weights
+            self._attention_weights[1][
+                i] = blk.attention2.attention.attention_weights
+        return self.dense(X), state
+
+    @property
+    def attention_weights(self):
+        return self._attention_weights
