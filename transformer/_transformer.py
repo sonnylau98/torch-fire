091ec1885f9f30d9c64e77a732dd5517ee07f742
sonnylau98 <liujingze00@gmail.com>
2023-12-27 07:52:59 +0000

    Update model

--- transformer/_transformer.py
+++ transformer/_transformer.py
@@ -10,29 +10,7 @@ from torchvision import transforms
 
 from PIL import Image
 
-################################################
-class PositionWiseFFN(nn.Module):
-    """基于位置的前馈网络"""
-    def __init__(self, ffn_num_input, ffn_num_hiddens, ffn_num_outputs,
-                 **kwargs):
-        super(PositionWiseFFN, self).__init__(**kwargs)
-        self.dense1 = nn.Linear(ffn_num_input, ffn_num_hiddens)
-        self.relu = nn.ReLU()
-        self.dense2 = nn.Linear(ffn_num_hiddens, ffn_num_outputs)
 
-    def forward(self, X):
-        return self.dense2(self.relu(self.dense1(X)))
-
-class AddNorm(nn.Module):
-    """残差连接后进行层规范化"""
-    def __init__(self, normalized_shape, dropout, **kwargs):
-        super(AddNorm, self).__init__(**kwargs)
-        self.dropout = nn.Dropout(dropout)
-        self.ln = nn.LayerNorm(normalized_shape)
-
-    def forward(self, X, Y):
-        return self.ln(self.dropout(Y) + X)
-################################################
 
 
 class Encoder(nn.Module):
--- transformer/model/encoder.py
+++ transformer/model/encoder.py
@@ -1,4 +1,42 @@
+from torch.nn import MultiheadAttention
+
+################################################
+class PositionWiseFFN(nn.Module):
+    """The positionwise feed-forward network."""
+    def __init__(self, ffn_num_input, ffn_num_hiddens, ffn_num_outputs,
+                 **kwargs):
+        super(PositionWiseFFN, self).__init__(**kwargs)
+        self.dense1 = nn.Linear(ffn_num_input, ffn_num_hiddens)
+        self.relu = nn.ReLU()
+        self.dense2 = nn.Linear(ffn_num_hiddens, ffn_num_outputs)
+
+    def forward(self, X):
+        return self.dense2(self.relu(self.dense1(X)))
+
+class AddNorm(nn.Module):
+    """The residual connection followed by layer normalization."""
+    def __init__(self, normalized_shape, dropout, **kwargs):
+        super(AddNorm, self).__init__(**kwargs)
+        self.dropout = nn.Dropout(dropout)
+        self.ln = nn.LayerNorm(normalized_shape)
+
+    def forward(self, X, Y):
+        return self.ln(self.dropout(Y) + X)
+################################################
+
 class TransformerEncoderBlock(nn.Module):
+  def __init__(self, num_hiddens, ffn_num_hiddens, num_heads, dropout,
+               use_bias=False):
+    super().__init__()
+    self.attention = MultiheadAttention(num_hiddens, num_heads,
+                                        dropout, use_bias)
+    self.addnorm1 = AddNorm(num_hiddens, dropout)
+    self.ffn = PositionWiseFFN(ffn_num_hiddens, num_hiddens)
+    self.addnorm2 = AddNorm(num_hiddens, dropout)
+
+  def forward(self, X, valid_lens):
+    Y = self.addnorm1(X, self.attention(X, X, X, valid_lens))
+    return self.addnorm2(Y, self.ffn(Y))
 
 class TransformerEncoder(Encoder):
   def __init__(self, vocab_size, num_hiddens, ffn_num_hiddens,
